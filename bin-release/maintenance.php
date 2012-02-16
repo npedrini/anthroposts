470c91f07d4211a4ff8ac71f536dc1151760759c
Noah Pedrini <go@looklisten.net>
2012-02-16 12:28:50 -0500

    adding bin-release folder

---
+++ bin-release/maintenance.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>AnthroPosts</title>
+
+<link rel="stylesheet" type="text/css" href="../styles.css" />
+
+</head>
+
+<body>
+<div style='margin-bottom: 20px;'>
+	<img src='../images/logo_splash.png' />
+</div>
+
+<h1>AnthroPosts is undergoing maintenance</h1>
+<h2>Don't worry, we'll be back shortly!</h2>
+</body>
+</html>
